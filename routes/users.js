39edd294f7db9a4910efdaf8d8bce56aa3c1af3c
yeswanthkanakala07 <82636668+yeswanthkanakala07@users.noreply.github.com>
2023-10-24 14:21:00 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var sum = 0;  // Initialize sum to 0
+var more = 0; // Initialize more to 0
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
+router.get('/', function(req, res, next) {
+  more += 1;  // Add 1 to more
+  sum += more; // Add the updated value of more to sum
+  res.send(`Sum is: ${sum}`); // Send the response with the updated sum
+
+});
 module.exports = router;
